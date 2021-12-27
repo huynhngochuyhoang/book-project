26b255a5bafcd96b8cd74bbc0c3fa5c805bd50bc
Hoang Huynh <huynhngochuyhoang@gmail.com>
2021-12-27 10:59:37 +0700

    added scripts

---
+++ runk8s.sh
@@ -0,0 +1,7 @@
+kubectl apply -f k8s/mysql.yml
+kubectl apply -f k8s/book-client.yml
+kubectl apply -f k8s/book-user.yml
+kubectl apply -f k8s/book-service.yml
+kubectl apply -f k8s/book-order.yml
+kubectl apply -f k8s/book-gateway.yml
+kubectl apply -f k8s/ingress.yml
